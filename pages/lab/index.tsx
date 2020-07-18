d2ac0ed67eb47b3c75adc812265dbee172491b83
Javier Carrillo Milla <stringparser@gmail.com>
2020-07-18 21:40:21 +0200

    audio: no outline for audio element; add proper lab index

---
+++ pages/lab/index.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+import List from "@material-ui/core/List";
+import Link from "@material-ui/core/Link";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import { makeStyles } from "@material-ui/core";
+
+
+type Props = {
+  items: Array<{
+    href: string;
+    title: string;
+  }>;
+  pageTitle: string;
+};
+
+export async function getStaticProps(): Promise<{ props: Props }> {
+  const fs = require('fs');
+  const path = require('path');
+
+  const items = fs.readdirSync(path.join(process.cwd(), 'pages', 'lab'))
+    .filter((el: string) => /\.mdx$/.test(el))
+    .map((el: string) => `/lab/${el.replace(/\.mdx$/, '')}`)
+    .map((href: string) => {
+      const title = href.split('/').pop() || '';
+
+      return {
+        href,
+        title: title.replace(/[-]/g, ' '),
+      };
+    })
+  ;
+
+  return {
+    props: {
+      items,
+      pageTitle: 'blog index',
+    },
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+const LabIndex: React.FC<Props> = ({
+  items = []
+}) => {
+  const classes = useStyles();
+
+  return (
+    <>
+      <h1>lab index</h1>
+
+      <List className={classes.root}>
+        {items.map((el, index) => {
+          return (
+            <ListItem key={index}>
+              <Link href={el.href}>
+                <ListItemText primary={el.title} />
+              </Link>
+            </ListItem>
+          );
+        })}
+      </List>
+    </>
+  );
+};
+
+export default LabIndex;
