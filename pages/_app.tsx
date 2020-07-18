6ceffd23363cc0855431f20f2675845b62e7d39a
Javier Carrillo Milla <stringparser@gmail.com>
2020-07-18 11:22:10 +0200

    Cleanup: fix empty AudioRecorder.tsx component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,9 +18,6 @@ class WebApp extends App<AppProps> {
       : {}
     ;
 
-    console.log(Component);
-    console.log('pageProps', pageProps);
-
     const meta = getMeta(router.route);
 
     return {
